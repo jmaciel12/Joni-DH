22ae9ee89046946838902e9678456d5bfc08084b
jmaciel12 <jmacielrampone@udesa.edu.ar>
2024-11-28 22:31:43 -0300

    Creación página category: html, js, mismos estilos que las recetas de la home

---
+++ js/category.js
@@ -0,0 +1,55 @@
+let querString = location.search;
+let querStringObj = new URLSearchParams(querString);
+let selectedTag = querStringObj.get("tag");
+console.log(selectedTag);
+
+if (selectedTag) {
+  document.querySelector(".title-categorias").textContent = selectedTag;
+
+  let url = `https://dummyjson.com/recipes/tag/${selectedTag}`;
+
+  fetch(url)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data.recipes);
+
+      let arrayDeRecetas = data.recipes;
+
+      let seccion = document.querySelector(".container-products");
+      let allRecipesHTML = "";
+
+      for (let i = 0; i < arrayDeRecetas.length; i++) {
+        allRecipesHTML += `
+          <article class="product-card">
+            <div class="product-details">
+              <img src="${arrayDeRecetas[i].image}" alt="${arrayDeRecetas[i].name}">
+              <p class="receta-name">${arrayDeRecetas[i].name}</p>
+              <p>Dificultad: ${arrayDeRecetas[i].difficulty}</p>
+            </div>
+            <div class="product-actions">
+              <a href="receta.html?id=${arrayDeRecetas[i].id}">Detalle</a>
+            </div>
+          </article>`;
+      }
+
+      seccion.innerHTML = allRecipesHTML;
+      const articles = document.querySelectorAll(".product-card");
+
+      for (let i = 0; i < articles.length; i++) {
+        const article = articles[i];
+        article.addEventListener("mouseover", function () {
+          article.style.transform = "scale(1.1)";
+        });
+        article.addEventListener("mouseout", function () {
+          article.style.transform = "scale(1)";
+        });
+      }
+    })
+    .catch(function (e) {
+      console.log("Error:", e);
+    });
+} else {
+  console.log("No se especificó un tag en la URL.");
+}
